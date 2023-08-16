d4c9c24196f0b4cd89ed84a78f7eaef7e0143b0a
XingHong <475773414@qq.com>
2023-08-16 19:16:24 +0800

    调整代码，测试性能

---
+++ Assets/Scripts/JsonVsFlexBuffers.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+using FlexBuffers;
+using UnityEngine.Profiling;
+using System.IO;
+
+public class JsonVsFlexBuffers : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Profiler.BeginSample("simple json import");
+        var txt = Resources.Load<TextAsset>("dataJson").text;
+        JSON.Parse(txt);
+        Profiler.EndSample();
+
+        Profiler.BeginSample("FlexBuffer import");
+        var bytes = Resources.Load<TextAsset>("data").bytes;
+        FlxValue val = FlxValue.FromBytes(bytes);
+        Profiler.EndSample();
+        //JSONArray arr = new JSONArray();
+        //for (long i = 0; i < 100000; ++i)
+        //{
+        //    arr[(int)i] = i;
+        //}
+
+        //Profiler.BeginSample("simple json read");
+        //List<long> list = new List<long>();
+        //for (int i = 0; i < 100000; ++i)
+        //{
+        //    list.Add(arr[i]);
+        //}
+        //Profiler.EndSample();
+
+        //var buffer = JsonToFlexBufferConverter.Convert(arr.ToString());
+        //FlxValue val = FlxValue.FromBytes(buffer);
+        //FlxVector vec = val.AsVector;
+
+        //Profiler.BeginSample("FlexBuffer read");
+        //List<long> list2 = new List<long>();
+        //for (int i = 0; i < 100000; ++i)
+        //{
+        //    list2.Add(vec[i].AsLong);
+        //}
+        //Profiler.EndSample();
+    }
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(0,0, 50, 30), "gen"))
+        {
+            JSONArray arr = new JSONArray();
+            for (long i = 0; i < 100000; ++i)
+            {
+                arr[(int)i] = i;
+            }
+
+            string path = Path.Combine(Application.dataPath, "Resource/data.json");
+            Debug.Log(path);
+            File.WriteAllText(path, arr.ToString());
+
+            var buffer = JsonToFlexBufferConverter.Convert(arr.ToString());
+            string bytesPath = Path.Combine(Application.dataPath, "Resource/data.txt");
+            Debug.Log(bytesPath);
+            File.WriteAllBytes(bytesPath, buffer);
+        }
+    }
+}
